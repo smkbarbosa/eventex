a3571265205c6f4faaad889eb6a19061cbfc4797
smkbarbosa <samuka1@gmail.com>
2017-06-06 16:14:00 -0300

    refatora geral

---
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -0,0 +1,12 @@
+from django.test.testcases import TestCase
+from eventex.subscriptions.forms import SubscriptionForm
+
+
+class SubscriptionFormTest(TestCase):
+    def setUp(self):
+        self.form = SubscriptionForm()
+
+    def test_fom_has_fields(self):
+        """Form must have 4 fields"""
+        expected = ['name', 'cpf', 'email', 'phone']
+        self.assertSequenceEqual(expected, list(self.form.fields))
---
+++ eventex/subscriptions/tests/test_mail_subscribe.py
@@ -0,0 +1,39 @@
+from django.core import mail
+from django.test import TestCase
+
+
+class SubscribePostValid(TestCase):
+    def setUp(self):
+        data = dict(name='Samuel Barbosa', cpf='12345678901',
+                    email='samuka1@gmail.com', phone='63-98137-3175')
+        self.resp = self.client.post('/inscricao/', data)
+        self.email = mail.outbox[0]
+
+    def test_subscription_email_subject(self):
+
+        expect = 'Confirmação de inscrição'
+
+        self.assertEqual(expect, self.email.subject)
+
+    def test_subscription_email_from(self):
+
+        expect = 'contato@eventex.com.br'
+
+        self.assertEqual(expect, self.email.from_email)
+
+    def test_subscription_email_to(self):
+
+        expect = ['contato@eventex.com.br', 'samuka1@gmail.com']
+
+        self.assertEqual(expect, self.email.to)
+
+    def test_subscription_email_body(self):
+        contents = [
+            'Samuel Barbosa',
+            '12345678901',
+            'samuka1@gmail.com',
+            '63-98137-3175',
+        ]
+        for content in contents:
+            with self.subTest():
+                self.assertIn(content, self.email.body)
