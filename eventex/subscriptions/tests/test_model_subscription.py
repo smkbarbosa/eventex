802c21d0f07dece8c135216202478d40ce7ea64e
Samuel Barbosa <samuka1@gmail.com>
2017-06-07 15:38:26 -0300

    Persiste a inscrição

---
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+
+from django.test import TestCase
+
+from eventex.subscriptions.models import Subscription
+
+
+class SubscriptionModelTest(TestCase):
+    def setUp(self):
+        self.obj = Subscription(
+            name = 'Samuel Barbosa',
+            cpf =' 12345678901',
+            email = 'contato@smkbarbosa.eti.br',
+            phone = '63-98137-3175'
+        )
+
+        self.obj.save()
+
+    def test_create(self):
+
+        self.assertTrue(Subscription.objects.exists())
+
+    def test_created_at(self):
+        """Subscription must have an auto created_at attr"""
+        self.assertIsInstance(self.obj.created_at, datetime)
