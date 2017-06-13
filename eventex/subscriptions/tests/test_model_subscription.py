f58aba59c6055babc95c645ea7f9f009c273c560
Samuel Barbosa <samuka1@gmail.com>
2017-06-13 16:55:40 -0300

    Adiciona campo paid

--- eventex/subscriptions/tests/test_model_subscription.py
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -26,3 +26,7 @@ class SubscriptionModelTest(TestCase):
 
     def test_str(self):
         self.assertEqual('Samuel Barbosa', str(self.obj))
+
+    def test_paid_default_to_False(self):
+        """By default paid must be False"""
+        self.assertEqual(False, self.obj.paid)
