c9b507ca1e5e3f9f8a9128b56ee5838c104658d4
Samuel Barbosa <samuka1@gmail.com>
2017-06-19 10:23:33 -0300

    Padroniza nomes

--- eventex/subscriptions/tests/test_form_subscription.py
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -24,6 +24,13 @@ class SubscriptionFormTest(TestCase):
 
         self.assertFormErrorCode(form, 'cpf', 'length')
 
+    def test_name_must_be_capitalized(self):
+        """Name must be capitalized"""
+        ## SAMUEL barbosa --> Samuel Barbosa
+
+        form = self.make_validated_form(name = 'SAMUEL barbosa')
+        self.assertEqual('Samuel Barbosa', form.cleaned_data['name'])
+
 
     def assertFormErrorCode(self, form, field, code ):
         errors = form.errors.as_data()
