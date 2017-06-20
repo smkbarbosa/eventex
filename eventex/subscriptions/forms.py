8bbe8a00e3fe27ad4054ff399bfdc22918403537
Samuel Barbosa <samuka1@gmail.com>
2017-06-20 11:11:09 -0300

    Simplifica com model forms

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -1,18 +1,12 @@
 from django import forms
 from django.core.exceptions import ValidationError
 
-
-def validate_cpf(value):
-    if not value.isdigit():
-        raise ValidationError('CPF deve conter apenas números', 'digits')
-
-    if len(value) != 11:
-        raise ValidationError('CPF deve ter 11 números', 'length')
+from eventex.subscriptions.models import Subscription
 
 
-class SubscriptionForm(forms.Form):
+class SubscriptionFormOld(forms.Form):
     name = forms.CharField(label='Nome')
-    cpf = forms.CharField(label='CPF', validators=[validate_cpf])
+    cpf = forms.CharField(label='CPF')
     email = forms.EmailField(label='E-mail', required=False)
     phone = forms.CharField(label='Telefone', required=False)
 
@@ -24,6 +18,27 @@ class SubscriptionForm(forms.Form):
         return ' '.join(words)
 
     def clean(self):
+        if not self.cleaned_data.get('email') and not self.cleaned_data.get('phone'):
+            raise ValidationError('Informe seu e-mail ou telefone.')
+
+        return self.cleaned_data
+
+
+class SubscriptionForm(forms.ModelForm):
+
+    class Meta:
+        model = Subscription
+        fields = ['name', 'cpf', 'email', 'phone']
+
+    def clean_name(self):
+        name = self.cleaned_data['name']
+
+        words = [w.capitalize() for w in name.split()]
+
+        return ' '.join(words)
+
+    def clean(self):
+        self.cleaned_data = super().clean() ## Chama o clean do Model
         if not self.cleaned_data.get('email') and not self.cleaned_data.get('phone'):
             raise ValidationError('Informe seu e-mail ou telefone.')
 
--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -26,7 +26,7 @@ def create(request):
         return render(request, 'subscriptions/subscription_form.html',
                       {'form': form})
 
-    subscription = Subscription.objects.create(**form.cleaned_data)
+    subscription = form.save()
 
     #Send Email
     _send_mail('Confirmação de inscrição',
